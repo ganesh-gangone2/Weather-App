1f6dbf92011639a5ea249b800f5f38c4001db8e9
Chakrapani <84615061+Chakrapani2122@users.noreply.github.com>
2022-10-20 20:38:48 +0530

    Update new-app.py

--- new-app.py
+++ new-app.py
@@ -1,3 +1,4 @@
+from math import floor
 import tkinter as tk
 from tkinter import *
 from turtle import update
@@ -8,8 +9,9 @@ import json
 from datetime import datetime
 
 root = Tk()
-root.title("Weather Now")
-
+root.title("Whats Weather Now")
+root.geometry("1300x700")
+root.resizable(False,False)
 
 try:
     def get_Weather():
@@ -45,11 +47,11 @@ try:
         temperature_data.after(100,update_temperature)
 
     def update_humidity():
-        humidity_data.config(text=data1["current"]["humidity"])
+        humidity_data.config(text=str(data1["current"]["humidity"]) + " %")
         humidity_data.after(100,update_humidity)
 
     def update_wind_speed():
-        wind_speed_data.config(text=data1["current"]["wind_speed"])
+        wind_speed_data.config(text=str(data1["current"]["wind_speed"]) + " meter/sec")
         wind_speed_data.after(100,update_wind_speed)
 
     def update_weather_desc():
@@ -61,11 +63,11 @@ try:
         time_zone_data.after(100,update_time_zone)
 
     def update_feelslike():
-        feelslike_data.config(text=data1["current"]["feels_like"])
+        feelslike_data.config(text=str(data1["current"]["feels_like"]) + " C")
         feelslike_data.after(100,update_feelslike)
 
     def update_pressure():
-        pressure_data.config(text=data1["current"]["pressure"])
+        pressure_data.config(text=str(data1["current"]["pressure"]) + " hPa")
         pressure_data.after(100,update_pressure)
 
     def update_uvi():
@@ -89,13 +91,15 @@ try:
 
 except:
     print("Unable to Fetch Data")
+    status_lbl = Label(root, text="Unabel to Fetch Data",font=("arial",20,"bold"))
+    status_lbl.place(x=200, y=300)
 
 get_Weather()
 
 # Adding Logo
 logo = PhotoImage(file="weather.png")
 logo_label = Label(root, image=logo)
-
+logo_label.place(x=50, y=100)
 
 # Adding Search Box
 Search_image = PhotoImage(file="search.png")
@@ -106,60 +110,74 @@ Search_icon = PhotoImage(file="search_icon.png")
 image_search_icon = Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040")
 
 # Adding Time Data
-digital_clock_lbl=Label(root,text="00.00.00",font=("ds-digital",20),fg="#404040")
-digital_clock_lbl.place(x=100,y=50)
+digital_clock_lbl=Label(root,text="00.00.00",font=("ds-digital",20))
+digital_clock_lbl.place(x=1100,y=50)
 update_clock()
 
 # Adding Date Data
 date = datetime.now()
-date_time_lbl = Label(root, text=f"{date:%d / %m / %Y\n%A}", font=("poppins", 10))
+date_time_lbl = Label(root, text=f"{date:%d / %m / %Y\n%A}", font=("poppins", 15))
+date_time_lbl.place(x=1100, y=90)
 update_date()
 
 # Adding Temperature Data
-temperature_data = Label(text=str(data1["current"]["temp"]) + " C", font=("arial",60,"bold"))
+temperature_data = Label(text=str(floor(data1["current"]["temp"])) + " C", font=("arial",60,"bold"))
+temperature_data.place(x=700,y=50)
 update_temperature()
 
 # Adding Time Zone Data 
 time_zone_data = Label(text=data1["timezone"], font=("arial",20,"bold"))
+time_zone_data.place(x=700,y=140)
 update_time_zone()
 
 # Adding Weather Description Data
 weather_desc_data = Label(text=data1["current"]["weather"][0]["main"].title(), font=("arial",20,"bold"))
+weather_desc_data.place(x=700,y=180)
 update_weather_desc()
 
 # Adding Wind Speed Label
-Wind_speed_lbl = Label(root, text="WIND", font= ("arial",15, "bold"),fg = "#404040")
+Wind_speed_lbl = Label(root, text="WIND              :", font= ("arial",15,"bold"))
+Wind_speed_lbl.place(x=700,y=300)
 
 # Adding Wind Speed Data
-wind_speed_data= Label(text= data1["current"]["wind_speed"], font=("arial",20,"bold"))
+wind_speed_data= Label(text= str(data1["current"]["wind_speed"]) + " meter/sec", font=("arial",15,"bold"))
+wind_speed_data.place(x=900,y=300)
 update_wind_speed()
 
 # Adding Humidity Label
-humidity_lbl = Label(root, text="HUMIDITY", font= ("poppins",15, "bold"),fg = "#404040")
+humidity_lbl = Label(root, text="HUMIDITY       :", font= ("poppins",15, "bold"))
+humidity_lbl.place(x=700,y=340)
 
 # Adding Humidity Data
-humidity_data = Label(text=data1["current"]["humidity"], font=("arial",20,"bold"))
+humidity_data = Label(text=str(data1["current"]["humidity"])+ "%", font=("arial",15,"bold"))
+humidity_data.place(x=900,y=340)
 update_humidity()
 
 # Adding Feels Like Label
-feelslike_lbl = Label(root, text="FELLS LIKE", font=("arial",15,"bold"),fg="#404040")
+feelslike_lbl = Label(root, text="FEELS LIKE    :", font=("arial",15,"bold"))
+feelslike_lbl.place(x=700,y=380)
 
 # Adding Feels Like Data
-feelslike_data = Label(root, text=data1["current"]["feels_like"],font=("arial",20,"bold"))
+feelslike_data = Label(root, text=str(data1["current"]["feels_like"]) + " C",font=("arial",15,"bold"))
+feelslike_data.place(x=900,y=380)
 update_feelslike()
 
 # Adding Pressure Label
-pressure_lbl = Label(root, text="PRESSURE", font=("arial",15,"bold"),fg="#404040")
+pressure_lbl = Label(root, text="PRESSURE     :", font=("arial",15,"bold"))
+pressure_lbl.place(x=700,y=420)
 
 # Adding Pressure Data
-pressure_data = Label(root, text=data1["current"]["pressure"],font=("arial",20,"bold"))
+pressure_data = Label(root, text=str(data1["current"]["pressure"]) + " hPa",font=("arial",15,"bold"))
+pressure_data.place(x=900,y=420)
 update_pressure()
 
 # Adding UVI Label
-uvi_lbl = Label(root, text="UVI", font=("arial",15,"bold"),fg="#404040")
+uvi_lbl = Label(root, text="UVI                  :", font=("arial",15,"bold"))
+uvi_lbl.place(x=700,y=460)
 
 # Adding UVI Data
-uvi_data = Label(root, text=data1["current"]["uvi"],font=("arial",20,"bold"))
+uvi_data = Label(root, text=data1["current"]["uvi"],font=("arial",15,"bold"))
+uvi_data.place(x=900,y=460)
 update_uvi()
 update_Weather()
 root.mainloop()
