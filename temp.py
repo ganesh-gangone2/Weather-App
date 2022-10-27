930315292a95bfe9f628584f820144b30dab62a3
Chakrapani <chakrapani.g99@gmail.com>
2022-10-27 19:02:34 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -13,3 +13,22 @@ for i in lis:
 print(location)
 
 # [[Hyderābād, Telangana, IN]]
+
+
+# icon id
+icon_id = data1["current"]["weather"][0]["icon"]
+# icon url
+icon_url = "http://openweathermap.org/img/wn/" + icon_id + ".png"
+# Downloading icon
+urllib.request.urlretrieve(icon_url, "icon.png")
+# Opening icon
+icon = Image.open("icon.png")
+# Resizing icon
+icon = icon.resize((100, 100), Image.ANTIALIAS)
+# Saving icon
+icon.save("icon.png")
+# Opening icon
+icon = Label(window,Image.open("icon.png"))
+# Displaying icon
+icon_label = Label(canvas, image=icon)
+icon_label.place(x=10, y=15)
