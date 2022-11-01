2e6f5aa2af250f8016f4837705592f8243cfac83
Chakrapani <84615061+Chakrapani2122@users.noreply.github.com>
2022-11-01 23:44:05 +0530

    Update weather-app.py

--- weather-app.py
+++ weather-app.py
@@ -46,7 +46,7 @@ try:
             cordinates = g.latlng
             global geo_position
             geo_position = cordinates
-            api_key = "8a7a7bedb7f79d4e68c2405b11725a61"
+            api_key = "Your API key"
             lat = cordinates[0]
             lon = cordinates[1]
             url1 = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
